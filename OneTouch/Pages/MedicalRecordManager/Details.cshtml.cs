2b27e56d5b453c666fa212901d896972644e0085
Nguyễn Gia Khánh <122737548+KhanhKube@users.noreply.github.com>
2025-06-06 08:27:21 +0700

    create dashboard

---
+++ OneTouch/Pages/MedicalRecordManager/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OneTouch.Models;
+
+namespace OneTouch.Pages.MedicalRecordManager
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly OneTouch.Models.OneTouchDbContext _context;
+
+        public DetailsModel(OneTouch.Models.OneTouchDbContext context)
+        {
+            _context = context;
+        }
+
+        public MedicalRecord MedicalRecord { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var medicalrecord = await _context.MedicalRecords.FirstOrDefaultAsync(m => m.RecordId == id);
+            if (medicalrecord == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                MedicalRecord = medicalrecord;
+            }
+            return Page();
+        }
+    }
+}
---
+++ OneTouch/Utils/enums/RoleEnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace OneTouch.Utils.enums
+{
+    public static class RoleEnumExtensions
+    {
+        public static string GetName(this RoleEnum role)
+        {
+            return role switch
+            {
+                RoleEnum.Admin => "Admin",
+                RoleEnum.Doctor => "Doctor",
+                RoleEnum.Patient => "Patient",
+                _ => role.ToString().ToUpper()
+            };
+        }
+    }
+}
